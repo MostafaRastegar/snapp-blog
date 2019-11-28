e931034478b7683523489aa38a3852c5da77eb28
mostafa rastegar <m.rastegar991@gmail.com>
2019-11-28 17:06:10 +0330

    Create Articlecard for article list page

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Loading from '../../components/snappLoading';
+import ArticleCard from '../../components/snappArticleCard';
 import { articlesAllGet } from '../../api/application/articles';
-import { Link } from 'react-router-dom';
+
 
 const HomePage = () => {
 	const [loading, setLoading] = useState(true);
@@ -26,19 +27,7 @@ const HomePage = () => {
 				<div className="row">
 					{articles.map((item) =>
 						<div key={item.slug} className="col-6">
-							<Link to={`/article/${item.slug}`}>
-								<div className="card mb-4">
-									<img className="card-img-top" src="http://placehold.it/750x300" alt="Card image cap" />
-									<div className="card-body">
-										<h2 className="card-title">{item.title}</h2>
-										<p className="card-text">{item.description}</p>
-									</div>
-									<div className="card-footer text-muted">
-										<span>by: {item.author.username}, </span>
-										<span>Date: {item.createdAt}</span>
-									</div>
-								</div>
-							</Link>
+							<ArticleCard data={item}/>
 						</div>
 					)}
 				</div> :
--- app/containers/TagsPage/index.js
+++ app/containers/TagsPage/index.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Loading from '../../components/snappLoading';
 import { articlesByTagGet } from '../../api/application/articles';
-import { Link } from 'react-router-dom';
-import './style.scss';
+import ArticleCard from '../../components/snappArticleCard';
 
 // eslint-disable-next-line react/prefer-stateless-function
 
@@ -30,19 +29,7 @@ const TagsPage = (props) => {
 				<div className="row">
 					{articles.map((item) =>
 						<div key={item.slug} className="col-6">
-							<Link to={`/article/${item.slug}`}>
-								<div className="card mb-4">
-									<img className="card-img-top" src="http://placehold.it/750x300" alt="Card image cap" />
-									<div className="card-body">
-										<h2 className="card-title">{item.title}</h2>
-										<p className="card-text">{item.description}</p>
-									</div>
-									<div className="card-footer text-muted">
-										<span>by: {item.author.username}, </span>
-										<span>Date: {item.createdAt}</span>
-									</div>
-								</div>
-							</Link>
+							<ArticleCard data={item}/>
 						</div>
 					)}
 				</div> :
